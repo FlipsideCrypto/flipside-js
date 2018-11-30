3d156c9998faf2ff819ac915f531b60a01c54d80
Jim Myers <jfmyers01@gmail.com>
2018-11-30 17:05:21 -0500

    Remove dist.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
+let version = require("./package.json").version;
+let filename = `flipside-v${version}.js`;
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "flipside.js",
+    filename: filename,
     path: path.resolve(__dirname, "dist")
   },
   mode: "development",
-
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
