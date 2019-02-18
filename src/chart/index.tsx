15291d473c2267090f6e14c1282d3ade43a344b0
Bryan Cinman <syngenius@gmail.com>
2019-02-18 16:14:29 -0500

    allow highcharts config in chart widget

--- src/chart/index.tsx
+++ src/chart/index.tsx
@@ -25,7 +25,6 @@ export type Props = {
   axisTitles?: string[];
   startDate?: string;
   endDate?: string;
-  colors?: string[];
   series: ChartSeries[];
   api: API;
 };
@@ -38,80 +37,95 @@ class Chart extends Component<Props> {
   container: HTMLElement;
 
   async componentDidMount() {
-    const { startDate, endDate, mode, colors } = this.props;
-    const apiSeries = createApiSeries(this.props.series);
-    const data = await this.props.api.fetchTimeseries({
+    const {
+      startDate,
+      endDate,
+      mode,
+      api,
+      series,
+      title,
+      ...rest
+    } = this.props;
+
+    const apiSeries = createApiSeries(series);
+    const data = await api.fetchTimeseries({
       series: apiSeries,
       start_date: startDate,
       end_date: endDate
     });
+
     const prefixes = zipObject(data.data.columns, data.data.prefixes) as {
       [k: string]: string;
     };
-    const series = createSeries(this.props.series, data.data, prefixes);
+    const loadedSeries = createSeries(series, data.data, prefixes);
+
     const textColor = mode === "dark" ? "#fff" : "#000";
     const tooltipBackground =
       mode === "dark" ? "rgba(0,0,0,0.7)" : "rgba(255,255,255,0.7)";
     const gridLineColor = mode === "dark" ? "#6c6c6c" : "#a3a3a3";
 
-    const options = merge({}, DEFAULT_HIGHCHARTS, {
-      series: series,
-      chart: {
-        renderTo: this.container
-      },
-      colors: colors,
-      title: {
-        text: this.props.title,
-        style: { color: textColor }
-      },
+    const options = merge(
+      {},
+      DEFAULT_HIGHCHARTS,
+      {
+        series: loadedSeries,
+        chart: {
+          renderTo: this.container
+        },
+        title: {
+          text: this.props.title,
+          style: { color: textColor }
+        },
 
-      legend: {
-        itemStyle: { color: textColor },
-        itemHoverStyle: { color: textColor }
-      },
+        legend: {
+          itemStyle: { color: textColor },
+          itemHoverStyle: { color: textColor }
+        },
 
-      tooltip: {
-        backgroundColor: tooltipBackground,
-        style: {
-          color: textColor
-        }
-      },
+        tooltip: {
+          backgroundColor: tooltipBackground,
+          style: {
+            color: textColor
+          }
+        },
 
-      rangeSelector: {
-        buttonTheme: {
-          states: {
-            select: {
-              style: {
-                color: mode === "dark" ? "#fff" : "#000"
+        rangeSelector: {
+          buttonTheme: {
+            states: {
+              select: {
+                style: {
+                  color: mode === "dark" ? "#fff" : "#000"
+                }
               }
             }
           }
-        }
-      },
+        },
 
-      xAxis: {
-        lineColor: gridLineColor,
-        tickColor: gridLineColor
-      },
+        xAxis: {
+          lineColor: gridLineColor,
+          tickColor: gridLineColor
+        },
 
-      yAxis: [
-        merge({}, DEFAULT_YAXIS, {
-          gridLineColor,
-          title: {
-            text: this.props.axisTitles[0],
-            style: { color: textColor }
-          }
-        }),
-        merge({}, DEFAULT_YAXIS, {
-          opposite: true,
-          gridLineColor,
-          title: {
-            text: this.props.axisTitles[1],
-            style: { color: textColor }
-          }
-        })
-      ]
-    });
+        yAxis: [
+          merge({}, DEFAULT_YAXIS, {
+            gridLineColor,
+            title: {
+              text: this.props.axisTitles[0],
+              style: { color: textColor }
+            }
+          }),
+          merge({}, DEFAULT_YAXIS, {
+            opposite: true,
+            gridLineColor,
+            title: {
+              text: this.props.axisTitles[1],
+              style: { color: textColor }
+            }
+          })
+        ]
+      },
+      rest
+    );
 
     Highcharts.chart(options);
   }
