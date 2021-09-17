837ff75f7e5caaa78bc964c56ae206532e85a4fd
Don Cote <mistercote@gmail.com>
2021-09-16 21:52:22 -0400

    remove messaging

--- src/components/noDataMessage/index.tsx
+++ src/components/noDataMessage/index.tsx
@@ -1,13 +1,6 @@
 import { h } from "preact";
 import * as css from "./style.css";
 
-const NoDataMessage = () => (
-  <p class={css.wrapper}>
-    Flipside Crypto does not currently track FCAS for this coin due to limited
-    data. In the event you are interested in discussing this coin, please
-    contact <a href="mailto:data@flipsidecrypto.com">data@flipsidecrypto.com</a>
-    .
-  </p>
-);
+const NoDataMessage = () => <div></div>;
 
 export default NoDataMessage;
